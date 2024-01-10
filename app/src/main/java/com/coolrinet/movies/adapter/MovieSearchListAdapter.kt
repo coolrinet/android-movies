4dfd284f4400783cd56bb3aff3a22b1f9b500362
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2024-01-10 18:25:00 +0700

    Create adapter for movie search list item

---
+++ app/src/main/java/com/coolrinet/movies/adapter/MovieSearchListAdapter.kt
@@ -0,0 +1,45 @@
+package com.coolrinet.movies.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.coolrinet.movies.R
+import com.coolrinet.movies.data.network.MovieSearchItem
+import com.coolrinet.movies.databinding.ListItemMovieSearchBinding
+
+class MovieSearchItemHolder(
+    private val binding: ListItemMovieSearchBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(
+        movieSearchItem: MovieSearchItem,
+        onMovieClicked: (MovieSearchItem) -> Unit,
+    ) {
+        binding.searchMovieTitle.text = movieSearchItem.title
+        binding.searchMovieYear.text = movieSearchItem.year
+        binding.searchMovieType.text = movieSearchItem.type
+
+        binding.searchMoviePoster.load(movieSearchItem.poster) {
+            placeholder(R.drawable.movie_poster_placeholder)
+        }
+    }
+}
+
+class MovieSearchListAdapter(
+    private val movieSearchList: List<MovieSearchItem>,
+    private val onMovieClicked: (MovieSearchItem) -> Unit,
+) : RecyclerView.Adapter<MovieSearchItemHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieSearchItemHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemMovieSearchBinding.inflate(inflater, parent, false)
+        return MovieSearchItemHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieSearchItemHolder, position: Int) {
+        val item = movieSearchList[position]
+        holder.bind(item, onMovieClicked)
+    }
+
+    override fun getItemCount() = movieSearchList.size
+
+}
--- app/src/main/java/com/coolrinet/movies/data/network/MovieSearchItem.kt
+++ app/src/main/java/com/coolrinet/movies/data/network/MovieSearchItem.kt
@@ -6,7 +6,7 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class MovieSearchItem(
     @Json(name = "Title") val title: String,
-    @Json(name = "Year") val year: Int,
+    @Json(name = "Year") val year: String,
     @Json(name = "Poster") val poster: String,
     @Json(name = "Type") val type: String,
 )
