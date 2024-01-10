d9f03d39524a4400e6980fc6d994e0e54c973999
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2024-01-10 19:53:17 +0700

    Fill recycler view in movie search list fragment

--- app/src/main/java/com/coolrinet/movies/fragment/MovieSearchListFragment.kt
+++ app/src/main/java/com/coolrinet/movies/fragment/MovieSearchListFragment.kt
@@ -5,9 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.coolrinet.movies.adapter.MovieSearchListAdapter
 import com.coolrinet.movies.databinding.FragmentMovieSearchListBinding
+import com.coolrinet.movies.viewmodel.MovieSearchListViewModel
+import com.coolrinet.movies.viewmodel.MovieSearchListViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.withCreationCallback
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MovieSearchListFragment : Fragment() {
@@ -16,6 +27,16 @@ class MovieSearchListFragment : Fragment() {
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
+
+    private val args: MovieSearchListFragmentArgs by navArgs()
+
+    private val viewModel: MovieSearchListViewModel by viewModels(
+        extrasProducer = {
+            defaultViewModelCreationExtras.withCreationCallback<MovieSearchListViewModelFactory> { factory ->
+                factory.create(args.searchQuery)
+            }
+        }
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,5 +48,27 @@ class MovieSearchListFragment : Fragment() {
         binding.movieSearchListRecyclerView.layoutManager = LinearLayoutManager(context)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.movieSearchList.collect {
+                    binding.movieSearchListRecyclerView.adapter =
+                        MovieSearchListAdapter(it) { _ ->
+                            findNavController().navigate(
+                                MovieSearchListFragmentDirections.addSelectedMovieToWatch()
+                            )
+                        }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
