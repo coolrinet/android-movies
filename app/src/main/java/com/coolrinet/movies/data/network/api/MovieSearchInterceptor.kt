bc6908c99fb27326639f6b0db4dd4f35fc0af7fa
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2024-01-10 15:15:59 +0700

    Create API interface for movie search

---
+++ app/src/main/java/com/coolrinet/movies/data/network/api/MovieSearchInterceptor.kt
@@ -0,0 +1,25 @@
+package com.coolrinet.movies.data.network.api
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+private const val API_KEY = "7c8e5b53"
+
+class MovieSearchInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+
+        val newUrl: HttpUrl = originalRequest.url.newBuilder()
+            .addQueryParameter("api_key", API_KEY)
+            .addQueryParameter("r", "json")
+            .build()
+
+        val newRequest: Request = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+}
---
+++ app/src/main/java/com/coolrinet/movies/data/network/api/OmdbApi.kt
@@ -0,0 +1,10 @@
+package com.coolrinet.movies.data.network.api
+
+import com.coolrinet.movies.data.network.OmdbResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OmdbApi {
+    @GET
+    suspend fun searchMovies(@Query("s") query: String): OmdbResponse
+}
